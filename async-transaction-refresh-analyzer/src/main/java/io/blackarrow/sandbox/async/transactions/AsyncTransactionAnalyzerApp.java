9d26f28d2cd6374484b7ff4d044be21d612330cf
Keith McNeill <k.mcneill@blackarrowgroup.io>
2023-01-19 10:45:12 +0000

    Fixed runtime arguments issue

--- async-transaction-refresh-analyzer/src/main/java/io/blackarrow/sandbox/async/transactions/AsyncTransactionAnalyzerApp.java
+++ async-transaction-refresh-analyzer/src/main/java/io/blackarrow/sandbox/async/transactions/AsyncTransactionAnalyzerApp.java
@@ -2,43 +2,43 @@ package io.blackarrow.sandbox.async.transactions;
 
 import io.blackarrow.sandbox.async.transactions.service.AsyncTransactionsProcessingService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import software.amazon.awssdk.regions.Region;
 
-import java.awt.event.WindowFocusListener;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
-import java.util.Arrays;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 
 @Slf4j
 @SpringBootApplication
-public class AsyncTransactionAnalyzerApp implements CommandLineRunner {
+public class AsyncTransactionAnalyzerApp implements ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(AsyncTransactionAnalyzerApp.class, args);
     }
 
     public static final File BASE_DIR;
+
     static {
         BASE_DIR =
                 new File(System.getProperty("user.home").concat("/BlackArrow/TransactionsRefreshSummaries"));
     }
+
     private static final String ARG_DATE = "date";
     private static final String ARG_OUTPUT_DIR = "output_dir";
     private static final Map<String, Region> ENV_MAP = Map.of("demo", Region.EU_WEST_2, "test", Region.US_EAST_2);
 
     @Override
-    public void run(String... args) {
-        Map<String, String> argMap = getArguments(args);
-        File outputDir = getOutputDirectory(argMap);
-        LocalDate date = getDateToUse(argMap);
+    public void run(ApplicationArguments args) {
+        File outputDir = getOutputDirectory(args);
+        LocalDate date = getDateToUse(args);
+
         AsyncTransactionsProcessingService asyncTransactionsProcessingService = new AsyncTransactionsProcessingService(outputDir);
 
         log.info("Checking AsyncTransactions for date {}, with output directory {}", date, outputDir);
@@ -53,11 +53,11 @@ public class AsyncTransactionAnalyzerApp implements CommandLineRunner {
         logSuccess(outputDir);
     }
 
-    private static LocalDate getDateToUse(Map<String, String> argMap) {
+    private static LocalDate getDateToUse(ApplicationArguments argMap) {
         LocalDate date = LocalDate.now().minusDays(0); // Default
-        if (argMap.containsKey(ARG_DATE)) {
+        if (argMap.containsOption(ARG_DATE) && !argMap.getOptionValues(ARG_DATE).isEmpty()) {
             try {
-                date = LocalDate.parse(argMap.get(ARG_DATE));
+                date = LocalDate.parse(argMap.getOptionValues(ARG_DATE).get(0));
             } catch (DateTimeParseException ex) {
                 log.error("There was a problem with the inout 'date' argument: ", ex);
                 System.exit(1);
@@ -67,20 +67,14 @@ public class AsyncTransactionAnalyzerApp implements CommandLineRunner {
         return date;
     }
 
-    private File getOutputDirectory(Map<String, String> argMap) {
+    private File getOutputDirectory(ApplicationArguments argMap) {
         File outputDir = BASE_DIR;
-        if (argMap.containsKey(ARG_OUTPUT_DIR)) {
-            outputDir = new File(argMap.get(ARG_OUTPUT_DIR));
+        if (argMap.containsOption(ARG_OUTPUT_DIR) && !argMap.getOptionValues(ARG_OUTPUT_DIR).isEmpty()) {
+            outputDir = new File(argMap.getOptionValues(ARG_OUTPUT_DIR).get(0));
         }
         return outputDir;
     }
 
-    private static Map<String, String> getArguments(String[] args) {
-        Map<String, String> argMap = Arrays.stream(args).map(arg -> arg.split("="))
-                .collect(Collectors.toMap(arr -> arr[0],
-                        arr -> arr.length > 1 ? arr[1] : ""));
-        return argMap;
-    }
 
     private static void logSuccess(File outputDir) {
         log.info("\n=================================================================================\n" +
